41544624f6f7009fab8eaacc69f46b9351c80db6
Brandon Abela <brandonabela9@gmail.com>
2018-03-15 19:27:40 +0100

    Add Subtract and Multiply Functions

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,4 +1,14 @@
 package calculator;
 
-public class Calculator {
+public class Calculator
+{
+    public int subtract (int value1, int value2)
+    {
+        return value2 - value1;
+    }
+
+    public int multiply (int value1, int value2)
+    {
+        return value2 * value1;
+    }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,4 +1,40 @@
 package calculator;
 
-public class CalculatorTest {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest
+{
+    private Calculator calculator;
+
+    @Before
+    public void Setup()
+    {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void testSubtractPositiveValues()
+    {
+        Assert.assertEquals(5, calculator.subtract(5, 10));
+    }
+
+    @Test
+    public void testSubtractNegativeValues()
+    {
+        Assert.assertEquals(-15, calculator.subtract(5, -10));
+    }
+
+    @Test
+    public void testMultiplyPositiveValues()
+    {
+        Assert.assertEquals(24, calculator.multiply(3, 8));
+    }
+
+    @Test
+    public void testMultiplyNegativeValues()
+    {
+        Assert.assertEquals(-8, calculator.multiply(2, -4));
+    }
 }
