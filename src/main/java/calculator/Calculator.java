0db33b3f6bdd52bc3b280725e2b74181bff3ad88
Daniel Camilleri <daniel.a.camilleri.16@um.edu.mt>
2018-03-15 18:58:54 +0100

    Initial functions

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,4 +1,15 @@
 package calculator;
 
 public class Calculator {
+  public int add(int value1,int value2){
+      return value1+value2;
+  }
+
+  public int divide(int value1, int value2){
+      if(value1 == 0){
+          return 0;
+      }else{
+        return value2/value1;
+      }
+  }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,4 +1,38 @@
 package calculator;
 
+import com.sun.org.apache.xpath.internal.operations.Equals;
+import junit.framework.Assert;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert.*;
+
 public class CalculatorTest {
+    private Calculator calculator;
+    @Before
+    public void setup(){
+        calculator = new Calculator();
+    }
+
+    @After
+    public void cleanup(){
+        calculator = null;
+    }
+
+
+    @Test
+    public void addFuncTest(){
+        Assert.assertEquals(5,calculator.add(2,3));
+    }
+
+    @Test
+    public void divideFuncTest(){
+        Assert.assertEquals(3,calculator.divide(2,6));
+
+    }
+
+    @Test
+    public void divideZeraFuncTest(){
+        org.junit.Assert.assertEquals(0,calculator.divide(0,1));
+    }
 }
