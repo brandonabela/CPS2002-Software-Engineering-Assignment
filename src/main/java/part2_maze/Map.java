a4901ba95e7c597c0d4a9be563747b1ea844440d
Brandon Abela <brandonabela9@gmail.com>
2018-04-10 09:48:29 +0200

    Generate Map with Start Position

--- src/main/java/part2_maze/Map.java
+++ src/main/java/part2_maze/Map.java
@@ -1,6 +1,51 @@
 package part2_maze;
 
+import java.util.Random;
+
+enum TileType
+{
+    GRASS,
+    ERROR
+}
+
 public class Map
 {
+    private int sizeOfMap;
+    private TileType[][] mapDetail;
+    private Position mapStartPosition;
 
+    public boolean setMapSize(int xSize, int ySize)
+    {
+        if (xSize == ySize)
+        {
+            this.sizeOfMap = xSize;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void generate()
+    {
+        mapDetail = new TileType[sizeOfMap][sizeOfMap];
+
+        Random rand = new Random();
+        mapStartPosition = new Position(rand.nextInt(sizeOfMap) + 1, rand.nextInt(sizeOfMap) + 1);
+
+        mapDetail[mapStartPosition.getYCoordinate()][mapStartPosition.getXCoordinate()] = TileType.GRASS;
+    }
+
+    public TileType getTileType(int xCoordinate, int yCoordinate)
+    {
+        if (mapDetail.length >= yCoordinate && mapDetail[0].length >= xCoordinate)
+        {
+            return mapDetail[yCoordinate][yCoordinate];
+        }
+        else
+        {
+            return TileType.ERROR;
+        }
+    }
 }
