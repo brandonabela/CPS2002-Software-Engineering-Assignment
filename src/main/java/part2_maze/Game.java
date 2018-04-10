d399770e846a24b832fcf8b3a904ffa8011eaede
Brandon Abela <brandonabela9@gmail.com>
2018-04-10 10:18:08 +0200

    Resolve Merge Conflict

--- src/main/java/part2_maze/Game.java
+++ src/main/java/part2_maze/Game.java
@@ -7,36 +7,38 @@ public class Game
     private int player_count;
     private int table_dimension;
     private int turns;
-    Player players[];
-    Map map;
+    private Player players[];
+    private Map map;
 
-
-
-    public Game(int player_count,int table_dimension){
+    private Game(int player_count, int table_dimension)
+    {
         this.player_count = player_count;
         this.table_dimension = table_dimension;
-
     }
 
-    void startGame(){
-        if(player_count >=2 && player_count <=4 && table_dimension >= 5 && table_dimension <= 50)
+    private void startGame()
+    {
+        if(player_count >= 2 && player_count <= 4 && table_dimension >= 5 && table_dimension <= 50)
         {
-            System.out.println("Player Count: "+player_count+"\n"+"Map Dimension: "+table_dimension+"x"+table_dimension);
-        }else if(player_count >=5 && player_count <=8 && table_dimension >= 8 && table_dimension <= 50){
-            System.out.println("Player Count: "+player_count+"\n"+"Map Dimension: "+table_dimension+"x"+table_dimension);
-
-        }else
+            System.out.println("Player Count: " + player_count + "\n" + "Map Dimension: " + table_dimension + "x" + table_dimension);
+        }
+        else if(player_count >= 5 && player_count <= 8 && table_dimension >= 8 && table_dimension <= 50)
+        {
+            System.out.println("Player Count: "+ player_count + "\n" + "Map Dimension: " + table_dimension + "x" + table_dimension);
+        }
+        else
+        {
             System.out.println("Error Incorrect dimensions or player count");
-
-
-
+        }
     }
 
     public static void main(String args[]){
 
         Scanner sc = new Scanner(System.in);
+
         int player_number;
         int table_size;
+
         System.out.println("Please enter the number of players");
         player_number = sc.nextInt();
 
@@ -45,9 +47,5 @@ public class Game
 
         Game game = new Game(player_number,table_size);
         game.startGame();
-
-
-
     }
-
 }
