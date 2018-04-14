6093f67e41ef2504ab1f95ce1b2103dd8a8b1f0b
Brandon Abela <brandonabela9@gmail.com>
2018-04-14 14:41:46 +0200

    Arranged Map Test File

--- src/test/java/part2_maze/MapTest.java
+++ src/test/java/part2_maze/MapTest.java
@@ -4,59 +4,60 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-
 public class MapTest
 {
     private Map map;
+
     @Before
-    public void setup(){
+    public void setup()
+    {
         map = new Map();
     }
 
-    @After
-    public void cleanup(){
-        map = null;
-
+    @Test
+    public void MapSize_Correct_test()
+    {
+        assertTrue(map.setMapSize(5, 5));
     }
 
     @Test
-    public void MapSize_Correct_test(){
-        assertEquals(true,map.setMapSize(5,5));
+    public void MapSize_Incorrect_test()
+    {
+        assertFalse(map.setMapSize(4, 5));
     }
 
     @Test
-    public void MapSize_Incorrect_test(){
-        assertEquals(false,map.setMapSize(4,5));
-
-    }
-
-
-    @Test
-    public void Generate_map_size_test(){
+    public void Generate_map_size_test()
+    {
         map.setMapSize(5,5);
         map.generate();
         TileType[][] game_map = map.getMapDetail();
 
         assertEquals(5,game_map.length);
 
-        for (TileType[] aGame_map : game_map) {
+        for (TileType[] aGame_map : game_map)
+        {
             assertEquals(5, aGame_map.length);
         }
 
     }
 
     @Test
-    public void GetTileType_SingleTile_Correct_test(){
+    public void GetTileType_SingleTile_Correct_test()
+    {
         map.setMapSize(5,5);
         map.generate();
         TileType tileType = map.getTileType(0,0);
-        if(tileType == TileType.ERROR)
-            fail();
 
+        if(tileType == TileType.ERROR)
+        {
+            fail();
+        }
     }
 
     @Test
-    public void GetTileType_SingleTile_Incorrect_test(){
+    public void GetTileType_SingleTile_Incorrect_test()
+    {
         map.setMapSize(5,5);
         map.generate();
         TileType tileType = map.getTileType(0,6);
@@ -64,25 +65,32 @@ public class MapTest
     }
 
     @Test
-    public void GetTiletoString_Grass_test(){
+    public void GetTileToString_Grass_test()
+    {
         assertEquals("grassTile",map.tileToString(TileType.GRASS));
     }
 
     @Test
-    public void GetTiletoString_Water_test(){
-        assertEquals("waterTile",map.tileToString(TileType.WATER));
+    public void GetTileToString_Water_test()
+    {
+        assertEquals("waterTile", map.tileToString(TileType.WATER));
     }
 
     @Test
-    public void GetTiletoString_Treasure_test(){
-        assertEquals("treasureTile",map.tileToString(TileType.TREASURE));
+    public void getTileToString_Treasure_test()
+    {
+        assertEquals("treasureTile", map.tileToString(TileType.TREASURE));
     }
+
     @Test
-    public void GetTiletoString_Error_test(){
-        assertEquals("unknownTile",map.tileToString(TileType.ERROR));
+    public void getTileToString_Error_test()
+    {
+        assertEquals("unknownTile", map.tileToString(TileType.ERROR));
     }
 
-
-
+    @After
+    public void cleanup()
+    {
+        map = null;
+    }
 }
-
