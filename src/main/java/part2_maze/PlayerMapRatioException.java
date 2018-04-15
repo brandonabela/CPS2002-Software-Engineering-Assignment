a6e8a3b51aeed0374cd52737eb88bcace9687093
Brandon Abela <brandonabela9@gmail.com>
2018-04-15 03:20:56 +0200

    Add Player Map Ratio Exception Comments

--- src/main/java/part2_maze/PlayerMapRatioException.java
+++ src/main/java/part2_maze/PlayerMapRatioException.java
@@ -1,7 +1,18 @@
 package part2_maze;
 
+/**
+ * A class which is responsible for defining a custom exception to be called every time the player inputs an
+ * incorrect ratio between the player and the map
+ */
 class PlayerMapRatioException extends Exception
 {
+    /**
+     * Constructor which is responsible for storing an exception which is throw every time the player
+     * inputs an incorrect ratio between the player and the map
+     *
+     * @param amountOfPlayers the amount of players to be played
+     * @param mapSize the size of the map
+     */
     PlayerMapRatioException(int amountOfPlayers, int mapSize)
     {
         super("The map size of " + mapSize + " x " + mapSize + " is not allowed for " + amountOfPlayers + " players");
