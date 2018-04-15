82c4ef3165b4f40aa09c5947f259a9ae3b4cc823
Brandon Abela <brandonabela9@gmail.com>
2018-04-15 03:21:18 +0200

    Add Position Comments

--- src/main/java/part2_maze/Position.java
+++ src/main/java/part2_maze/Position.java
@@ -1,15 +1,26 @@
 package part2_maze;
 
+/**
+ * Class which is responsible for defining the positions within the game
+ */
 public class Position
 {
-    private int xCoordinate;
-    private int yCoordinate;
+    private int xCoordinate;    // The x value moving left or right
+    private int yCoordinate;    // The y value moving up or down
 
+    /**
+     * Constructor which defines the position object
+     *
+     * @param xCoordinate the x coordinate to be assigned
+     * @param yCoordinate the y coordinate to be assigned
+     */
     Position(int xCoordinate, int yCoordinate)
     {
         this.xCoordinate = xCoordinate;
         this.yCoordinate = yCoordinate;
     }
+
+    // Get Methods
 
     int getXCoordinate()
     {
@@ -21,6 +32,8 @@ public class Position
         return yCoordinate;
     }
 
+    // Set Methods
+
     void setXCoordinate(int xCoordinate)
     {
         this.xCoordinate = xCoordinate;
@@ -31,6 +44,11 @@ public class Position
         this.yCoordinate = yCoordinate;
     }
 
+    /**
+     * Method which is responsible for overriding the default behaviour of the toString method
+     *
+     * @return a string based on the given return string format
+     */
     @Override
     public String toString()
     {
